417528837e8ae7d0bd012306a43314de5a3903d6
eibounda <bryan.elvis.eb@gmail.com>
2024-06-27 01:48:03 +0000

    ajout des directions des vehicules

--- src/event.rs
+++ src/event.rs
@@ -1,13 +1,13 @@
 pub use sdl2::event::Event;
 pub use sdl2::keyboard::Keycode;
-
+use crate::DirectionPath;
 use crate::Position;
 use crate::Vehicle;
 use crate::Direction;
 use std::rc::Rc;
-
+use sdl2::messagebox::*;
 use sdl2::render::*;
-
+pub use rand::Rng;
 pub struct Simulation<'a> {
     pub vehicles: Vec<Vehicle<'a>>,
     pub next_id: u32,
@@ -22,34 +22,42 @@ impl<'a> Simulation<'a> {
     }
 
     pub fn handle_event(&mut self, event: &Event, texture: Rc<Texture<'a>>) {
+        let mut rng = rand::thread_rng();
+        let path = match rng.gen_range(0..=2) {
+            0 => DirectionPath::GoStraight,
+            1 => DirectionPath::TurnLeft,
+            2 => DirectionPath::TurnRight,
+            _ => DirectionPath::GoStraight, // Par défaut
+        };
         match event {
             Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
-                self.create_vehicle(Direction::North, texture);
+                self.create_vehicle(Direction::North(path), texture, path);
             }
             Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
-                self.create_vehicle(Direction::South, texture);
+                self.create_vehicle(Direction::South(path), texture, path);
             }
             Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
-                self.create_vehicle(Direction::West, texture);
+                self.create_vehicle(Direction::West(path), texture, path);
             }
             Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
-                self.create_vehicle(Direction::East, texture);
+                self.create_vehicle(Direction::East(path), texture, path);
             }
             Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                self.print_statistics();
                 std::process::exit(0);
+                // self.print_statistics();
             }
             _ => {}
         }
     }
 
-    pub fn create_vehicle(&mut self, direction: Direction, texture: Rc<Texture<'a>>) {
-        let position = match direction {
-            Direction::North => Position{x:400.0, y:800.0},
-            Direction::South => Position{x:400.0, y:0.0},
-            Direction::East  => Position{x:0.0, y:400.0},
-            Direction::West  => Position{x:800.0, y:400.0},
-            _ => Position{x:0.0, y:0.0},
+    pub fn create_vehicle(&mut self, direction: Direction, texture: Rc<Texture<'a>>, _path: DirectionPath) {
+        
+        let (position, _dir) = match direction {
+            Direction::North(path) => (Position{x:443.0, y:800.0}, Direction::North(path)), // ↑
+            Direction::South(path) => (Position{x:310.0, y:0.0}, Direction::South(path)), // ↓
+            Direction::East(path)  => (Position{x:0.0, y:422.0}, Direction::East(path)), // →
+            Direction::West(path)  => (Position{x:800.0, y:290.0}, Direction::West(path)), // ←
+            // _ => Position{x:0.0, y:0.0},
         };
         let vehicle = Vehicle::new(self.next_id, position, direction, texture);
         self.vehicles.push(vehicle);
@@ -57,8 +65,8 @@ impl<'a> Simulation<'a> {
     }
 
     pub fn update(&mut self) {
-        for vehicle in &mut self.vehicles {
-            vehicle.update_position();
+        for vehicle in &mut self.vehicles.clone() {
+            vehicle.update_position(self.vehicles.clone());
         }
     }
 
@@ -72,18 +80,26 @@ impl<'a> Simulation<'a> {
     }
 
     pub fn print_statistics(&self) {
-        println!("Nombre de véhicules: {}", self.vehicles.len());
+        let mut statistics = String::new();
+        statistics.push_str(&format!("Nombre de véhicules: {}\n", self.vehicles.len()));
         if let Some(max_velocity) = self.vehicles.iter().max_by(|a, b| a.velocity.partial_cmp(&b.velocity).unwrap()) {
-            println!("Vitesse maximale: {}", max_velocity.velocity);
+            statistics.push_str(&format!("Vitesse maximale: {}\n", max_velocity.velocity));
         }
         if let Some(min_velocity) = self.vehicles.iter().min_by(|a, b| a.velocity.partial_cmp(&b.velocity).unwrap()) {
-            println!("Vitesse minimale: {}", min_velocity.velocity);
+            statistics.push_str(&format!("Vitesse minimale: {}\n", min_velocity.velocity));
         }
         if let Some(max_time) = self.vehicles.iter().max_by(|a, b| a.time_in_intersection.partial_cmp(&b.time_in_intersection).unwrap()) {
-            println!("Temps maximal dans l'intersection: {}", max_time.time_in_intersection);
+            statistics.push_str(&format!("Temps maximal dans l'intersection: {}\n", max_time.time_in_intersection));
         }
         if let Some(min_time) = self.vehicles.iter().min_by(|a, b| a.time_in_intersection.partial_cmp(&b.time_in_intersection).unwrap()) {
-            println!("Temps minimal dans l'intersection: {}", min_time.time_in_intersection);
+            statistics.push_str(&format!("Temps minimal dans l'intersection: {}\n", min_time.time_in_intersection));
         }
+        
+        show_simple_message_box(
+            MessageBoxFlag::INFORMATION,
+            "!!STATISTICS!!",
+            &statistics,
+            None,
+        ).unwrap();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 pub mod event;
-pub mod car;
 
 pub use std::rc::Rc;
 
-pub use rand::Rng;
 pub use sdl2::pixels::Color;
 pub use sdl2::rect::Rect;
 use sdl2::render::Texture;
 
+#[derive(Clone, Copy)]
 pub struct Position {
     pub x: f32,
     pub y: f32,
 }
 
+#[derive(Clone)]
 pub struct Vehicle<'a>{
     pub id: u32,
     pub position: Position,
@@ -20,13 +20,19 @@ pub struct Vehicle<'a>{
     pub direction: Direction,
     pub time_in_intersection: f32,
     texture: Rc<Texture<'a>>,
+    angle: f64, // Angle de rotation en degrés
 }
 
+#[derive(Clone, Copy)]
 pub enum Direction {
-    North,
-    South,
-    East,
-    West,
+    North(DirectionPath),
+    South(DirectionPath),
+    East(DirectionPath),
+    West(DirectionPath),
+}
+
+#[derive(Clone, Copy)]
+pub enum DirectionPath {
     TurnLeft,
     TurnRight,
     GoStraight,
@@ -34,29 +40,105 @@ pub enum Direction {
 
 impl<'a> Vehicle<'a> {
     pub fn new(id: u32, position: Position, direction: Direction, texture: Rc<Texture<'a>>) -> Self {
+        let angle = match direction {
+            Direction::West(_) => 270.0,
+            Direction::East(_) => 90.0,
+            Direction::North(_) => 0.0,
+            Direction::South(_) => 180.0,
+        };
         Vehicle {
             id,
             position,
             velocity: 1.0,
             direction,
             time_in_intersection: 0.0,
-            texture
+            texture,
+            angle
         }
     }
 
-    pub fn update_position(&mut self) {
+    pub fn update_position(&mut self, vehicles: Vec<Vehicle<'a>>) {
+        // match self.direction {
+        //     Direction::North(_) => self.position.y -= self.velocity,
+        //     Direction::South(_) => self.position.y += self.velocity,
+        //     Direction::East(_)  => self.position.x += self.velocity,
+        //     Direction::West(_)  => self.position.x -= self.velocity,
+        // }
         match self.direction {
-            Direction::North => self.position.y -= self.velocity,
-            Direction::South => self.position.y += self.velocity,
-            Direction::East  => self.position.x += self.velocity,
-            Direction::West  => self.position.x -= self.velocity,
-            _ => (),
+            Direction::North(path) => {
+                match path {
+                    DirectionPath::GoStraight => {
+                        for vehicule in vehicles{
+                            if vehicule.id != self.id{
+                                
+                            }
+                        }
+                        self.position.y -= self.velocity
+                    },
+                    DirectionPath::TurnLeft => {
+                        self.position.y -= self.velocity;
+                        self.position.x -= self.velocity; // Ou autre logique
+                    }
+                    DirectionPath::TurnRight => {
+                        self.position.y -= self.velocity;
+                        self.position.x += self.velocity; // Ou autre logique
+                    }
+                }
+            }
+            Direction::South(path) => {
+                match path {
+                    DirectionPath::GoStraight => self.position.y += self.velocity,
+                    DirectionPath::TurnLeft => {
+                        self.position.y += self.velocity;
+                        self.position.x += self.velocity; // Ou autre logique
+                    }
+                    DirectionPath::TurnRight => {
+                        self.position.y += self.velocity;
+                        self.position.x -= self.velocity; // Ou autre logique
+                    }
+                }
+            }
+            Direction::East(path) => {
+                match path {
+                    DirectionPath::GoStraight => self.position.x += self.velocity,
+                    DirectionPath::TurnLeft => {
+                        self.position.x += self.velocity;
+                        self.position.y -= self.velocity; // Ou autre logique
+                    }
+                    DirectionPath::TurnRight => {
+                        self.position.x += self.velocity;
+                        self.position.y += self.velocity; // Ou autre logique
+                    }
+                }
+            }
+            Direction::West(path) => {
+                match path {
+                    DirectionPath::GoStraight => self.position.x -= self.velocity,
+                    DirectionPath::TurnLeft => {
+                        self.position.x -= self.velocity;
+                        self.position.y += self.velocity; // Ou autre logique
+                    }
+                    DirectionPath::TurnRight => {
+                        self.position.x -= self.velocity;
+                        self.position.y -= self.velocity; // Ou autre logique
+                    }
+                }
+            }
         }
         self.time_in_intersection += 1.0;
     }
 
     pub fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
         let rect = Rect::new(self.position.x as i32, self.position.y as i32, 50, 100);
-        canvas.copy(&self.texture, None, rect).unwrap();
+        // canvas.copy(&self.texture, None, rect).unwrap();
+        canvas.copy_ex(
+            &self.texture,
+            None,
+            Some(rect),
+            self.angle,
+            None,
+            false,
+            false
+        ).unwrap();
     }
 }
